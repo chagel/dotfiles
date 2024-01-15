c636874797d1a141fd83bd57f9781bd40055dc0b
MGC <chagel@gmail.com>
2024-01-14 16:34:24 -0800

    add misc

---
+++ scripts/firefox-profiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Firefox - PIPI
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author MGC
+
+/Applications/Firefox.app/Contents/MacOS/firefox -P pipi
+
---
+++ scripts/wechat-plus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title WeChat+
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName us.gchen.wechat
+
+# Documentation:
+# @raycast.description Start another WeChat instance
+# @raycast.author MGC
+
+nohup /Applications/WeChat.app/Contents/MacOS/WeChat> /dev/null 2>&1 &
